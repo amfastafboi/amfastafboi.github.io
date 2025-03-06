815205a4dacbc99f668e66709f7281c020212eb5
Zaid Ibn Faisal <amfastafboi@gmail.com>
2025-03-06 10:11:23 +0530

    changes1

--- src/components/ToolboxItems.tsx
+++ src/components/ToolboxItems.tsx
@@ -15,7 +15,7 @@ export const ToolboxItems = ({ items, className, itemsWrapperClassName }: {
         <div className={twMerge("flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]", className)}>
             <div className={twMerge("flex py-0.5 flex-none gap-6 pr-6", itemsWrapperClassName)}>
             {[...new Array(2)].fill(0).map((_, idx) => (
-            <Fragment>
+            <Fragment key={idx}>
                 {items.map(item => (
                     <div key={item.title} className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg">
                         <TechIcon component={item.iconType} />
